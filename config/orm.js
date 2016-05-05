15cd359413956f93ecf805234424a151b5e7e3c2
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-05-04 20:42:08 -0400

    all pages and early routing

--- config/orm.js
+++ config/orm.js
@@ -3,7 +3,7 @@ var connection = require('./connection.js');
 var orm = {
 	// select all
 	selectAll : function(table, callback){
-		var script = "SELECT * from ?";
+		var script = "SELECT * from "+table;
 		connection.query(script, function(err, result){
 			callback(result);
 		})
@@ -12,14 +12,14 @@ var orm = {
 	giveHelp : function(name, phone, email, available){
 		var script = "INSERT INTO give_help (name, phone, email, available) VALUES(?, ?, ?, ?)";
 		connection.query(script, [name, phone, email, available], function(err, result){
-			console.log(result);
+			callback(result);
 		})
 	},
 	// add helpee (getHelp)
 	getHelp : function(name, phone, email, available, need_help){
 		var script = "INSERT INTO get_help (name, phone, email, available, need_help) VALUES(?, ?, ?, ?, ?)";
 		connection.query(script, [name, phone, email, available, need_help], function(err, result){
-			console.log(result);
+			callback(result);
 		})
 	}
 }
